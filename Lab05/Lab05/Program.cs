2a402dcf8bf1dcfe61d476edf35f812e4cd8b502
panwaraporn <58030219@kmitl.ac.th>
2017-11-06 20:30:08 +0700

    commit lan05-08-1

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -5,12 +5,12 @@ class Program
     static void Main()
     {
         int val = 999;
-        refMethod(ref val);
+        refMethod(out val);
         Console.WriteLine(val);
         Console.ReadLine();
     }
 
-    static void refMethod(ref int i)
+    static void refMethod(out int i)
     {
         i = i + 1;
     }
