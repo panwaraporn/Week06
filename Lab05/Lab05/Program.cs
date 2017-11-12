a80223775bf15cfa1576f72dea5d5f7b228dd016
panwaraporn <58030219@kmitl.ac.th>
2017-11-13 01:24:23 +0700

    commit lab05-09-3

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -1,21 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 class Program
 {
-    int x = 17;
-    double y = x / 4.0;
-    double z = y * y;
-    Console.WriteLine(y);
-Console.WriteLine(z);
-
-int a = 5;
-    int b = 4;
-    Console.WriteLine(a + b);	
-Console.WriteLine(a + (b++));  
-Console.WriteLine(a + b);	
-Console.WriteLine(a + (++b));  
-Console.WriteLine(a + b);	
-Console.WriteLine(14 / a); 	
-Console.WriteLine(14 % a);
-
+    static void Main(string[] args)
+    {
+        int x = 17;
+        double y = x / 4.0;
+        double z = y * y;
+        Console.WriteLine(y);
+        Console.WriteLine(z);
+    }
+}
+    class program2
+{ 
+    static void Main(string[] args)
+    { 
+            int a = 5;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (b++));
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (++b));
+            Console.WriteLine(a + b);
+            Console.WriteLine(14 / a);
+            Console.WriteLine(14 % a);
+     }
 }
