2a13d2b606aa2b5b09c603c0fe2d49875f9882bc
panwaraporn <58030219@kmitl.ac.th>
2017-11-13 02:38:54 +0700

    commit lab05-17_2

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,12 +4,18 @@ class Program
 {
     static void Main()
     {
-        int myInt = 5;
-        Console.WriteLine(myInt);
-        long myLong = myInt;
-        Console.WriteLine(myLong);
-        Console.WriteLine(myLong + myInt);
+        double myDouble = 9.9d;
+        Console.WriteLine(myDouble);
 
+        long myLong = (long)myDouble;
+        Console.WriteLine(myLong);
+
+        myDouble = 9e9d;
+        Console.WriteLine(myDouble);
+
+        int myInt = (int)myDouble;
+        Console.WriteLine(myInt);
+        Console.WriteLine(int.MinValue);
 
     }
 }
