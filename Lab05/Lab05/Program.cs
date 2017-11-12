0f3d412a9d94b0fb18562b3b155379c7fcbccbaa
panwaraporn <58030219@kmitl.ac.th>
2017-11-13 03:08:00 +0700

    commit lab05-20_3

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -5,8 +5,10 @@ class Program
     static void Main()
     {
         int a = 2;
-        string myString = a.ToString();
-        Console.WriteLine(myString);
+        float f = 3.5f;
+        double d = 4.99d;
+        decimal money = 1254841354;
+        Console.WriteLine(a + " " + f + " " + d + " " + money);
 
     }
 }
